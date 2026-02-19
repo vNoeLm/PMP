e31b1a61e24e0c84c387b567eca138d9ec9761b6
NoeŁ <124210421+vNoeLm@users.noreply.github.com>
2026-02-19 11:20:39 +0100

    szoftlabor1

---
+++ SzoftLabor1/SzoftLabor1/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzoftLabor1
+{
+    public enum Species
+    {
+        Dog,
+        Panda,
+        Rabbit
+    }
+    internal class Animal
+    {
+        string name;
+        bool gender;
+        int weight;
+        Species species;
+
+        public string Name { get => name;}
+        public bool Gender { get => gender;}
+        public int Weight { get => weight;}
+        public Species Species { get => species;}
+
+        public Animal(string name, bool gender, int weight, Species species)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.weight = weight;
+            this.species = species;
+        }
+
+        
+    }
+}
---
+++ SzoftLabor1/SzoftLabor1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SzoftLabor1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
